918c87e020c8fca0b69a300987f189355de78374
piyush016 <piyushcool116@gmail.com>
2024-02-13 04:06:29 +0530

    Fix: New Design, Status and Notes working

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
+import { collection, query, getDocs, setDoc } from "firebase/firestore";
 import {
   Table,
   TableBody,
@@ -6,21 +7,13 @@ import {
   TableContainer,
   TableHead,
   TableRow,
-  Checkbox,
-  IconButton,
-  InputBase,
   Typography,
+  Box,
 } from "@mui/material";
-import Typewriter from "typewriter-effect";
 import { styled } from "@mui/material/styles";
-import { green } from "@mui/material/colors";
-import { BsFillRocketFill } from "react-icons/bs";
-import { BiEdit } from "react-icons/bi";
-import { MdFileDownloadDone } from "react-icons/md";
+import { db } from "../../firebase";
 import RowQues from "./RowQues";
-import { dsatopics } from "./data";
-import { db, auth } from "../../firebase";
-import { doc, onSnapshot, setDoc, getDoc } from "firebase/firestore";
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
 
 const StyledTableContainer = styled(TableContainer)`
   margin-top: 16px;
@@ -30,220 +23,166 @@ const StyledTable = styled(Table)`
   min-width: 650px;
 `;
 
-const StyledTableRow = styled(TableRow)`
-  &:not(:last-child) {
-    border-bottom: 1px solid #e0e0e0;
-  }
-`;
-
 const TableView = ({ topic }) => {
   const [filteredData, setFilteredData] = useState([]);
-  const [editingNoteId, setEditingNoteId] = useState(null);
-  const [noteValue, setNoteValue] = useState("");
-  const unsubscribeRef = useRef(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    console.log("Selected Topic:", topic);
-    const filtered = dsatopics.filter((item) => item.dsatopic === topic);
-    setFilteredData(filtered);
-  }, [topic]);
-
-  useEffect(() => {
-    console.log("Selected Topic:", topic);
-    const fetchUserData = async () => {
+    let filteredQuestions = [];
+    const fetchQuestions = async () => {
       try {
-        const user = auth.currentUser;
-        if (user) {
-          const userId = user.uid;
-          const userDocRef = doc(db, "users", userId);
-          const snapshot = await getDoc(userDocRef);
-          const userData = snapshot.data();
+        setLoading(true);
+        const q = query(collection(db, "questions"));
+        const querySnapshot = await getDocs(q);
+        const questions = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        filteredQuestions = questions.filter(
+          (question) => question.dsatopic === topic
+        );
+        fetchUserData(filteredQuestions); // Call fetchUserData after setting filteredData
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching questions:", error);
+        setLoading(false);
+      }
+    };
 
-          const updatedData = dsatopics.map((item) => {
-            const matchingTopic = Array.isArray(userData.notes)
-              ? userData.notes.find((data) => data.id === item.id)
-              : null;
-            return {
-              ...item,
-              status: matchingTopic ? matchingTopic.status : false,
-              note: matchingTopic ? matchingTopic.note : "",
-            };
-          });
+    const fetchUserData = async (filteredQuestions) => {
+      try {
+        const userQ = query(collection(db, "users"));
+        const userQuerySnapshot = await getDocs(userQ);
+        const userData = userQuerySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }))[0];
 
-          setFilteredData(updatedData);
+        // Fetch questions relevant to the current topic
+        const filteredQuestionIds = filteredQuestions.map(
+          (question) => question.id
+        );
+        const relevantQuestions = userData.questions.filter((q) =>
+          filteredQuestionIds.includes(q.id)
+        );
 
-          unsubscribeRef.current = onSnapshot(userDocRef, (snapshot) => {
-            if (snapshot.exists()) {
-              const userData = snapshot.data();
+        // Merge status of relevant questions with filteredData
+        const updatedQuestions = filteredQuestions.map((question) => {
+          const userQuestion = relevantQuestions.find(
+            (q) => q.id === question.id
+          );
+          return {
+            ...question,
+            status: userQuestion ? userQuestion.status : false,
+            note: userQuestion ? userQuestion.note : "", // Add note if available
+          };
+        });
 
-              const updatedData = dsatopics.map((item) => {
-                const matchingTopic = Array.isArray(userData.notes)
-                  ? userData.notes.find((data) => data.id === item.id)
-                  : null;
-                return {
-                  ...item,
-                  status: matchingTopic ? matchingTopic.status : false,
-                  note: matchingTopic ? matchingTopic.note : "",
-                };
-              });
-
-              setFilteredData(updatedData);
-            }
-          });
-        }
+        setFilteredData(updatedQuestions);
       } catch (error) {
         console.error("Error fetching user data:", error);
       }
     };
 
-    fetchUserData();
+    fetchQuestions();
   }, [topic]);
 
-  const handleNoteClick = (id, note) => {
-    if (id === editingNoteId) {
-      setEditingNoteId(null);
-      setNoteValue("");
-    } else {
-      setEditingNoteId(id);
-      setNoteValue(note);
-    }
-  };
-
-  const handleNoteChange = (event) => {
-    setNoteValue(event.target.value);
-  };
-
-  const handleNoteSave = async (id) => {
-    const updatedData = filteredData.map((item) => {
-      if (item.id === id) {
-        return {
-          ...item,
-          note: noteValue,
-        };
-      }
-      return item;
-    });
-
-    setFilteredData(updatedData);
-    setEditingNoteId(null);
-    setNoteValue("");
-
+  // Function to handle status change
+  const handleStatusChange = async (id, newStatus) => {
     try {
-      const user = auth.currentUser;
-      if (user) {
-        const userId = user.uid;
-        await setDoc(
-          doc(db, "users", userId),
-          {
-            notes: updatedData.map((item) => ({
-              id: item.id,
-              note: item.note,
-            })),
-          },
-          { merge: true }
-        );
+      const userQ = query(collection(db, "users"));
+      const userQuerySnapshot = await getDocs(userQ);
+      const userData = userQuerySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }))[0];
+
+      const userDocRef = userQuerySnapshot.docs[0].ref;
+      const questionIndex = userData.questions.findIndex((q) => q.id === id);
+
+      if (questionIndex !== -1) {
+        userData.questions[questionIndex].status = newStatus;
+        await setDoc(userDocRef, { questions: userData.questions });
       }
     } catch (error) {
-      console.error("Error saving note:", error);
+      console.error("Error updating status:", error);
     }
   };
 
-  const handleStatusChange = async (id) => {
-    const updatedData = filteredData.map((item) => {
-      if (item.id === id) {
-        return {
-          ...item,
-          status: !item.status,
-        };
-      }
-      return item;
-    });
-
-    setFilteredData(updatedData);
-
+  // Function to handle note save
+  const handleNoteSave = async (id, newNote) => {
     try {
-      const user = auth.currentUser;
-      if (user) {
-        const userId = user.uid;
-        await setDoc(
-          doc(db, "users", userId),
-          {
-            notes: updatedData.map((item) => ({
-              id: item.id,
-              status: item.status,
-            })),
-          },
-          { merge: true }
-        );
+      const userQ = query(collection(db, "users"));
+      const userQuerySnapshot = await getDocs(userQ);
+      const userData = userQuerySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }))[0];
+
+      const userDocRef = userQuerySnapshot.docs[0].ref;
+      const questionIndex = userData.questions.findIndex((q) => q.id === id);
+
+      if (questionIndex !== -1) {
+        userData.questions[questionIndex].note = newNote;
+        await setDoc(userDocRef, { questions: userData.questions });
       }
     } catch (error) {
-      console.error("Error saving status:", error);
+      console.error("Error updating note:", error);
     }
   };
-
-  let currentTopic = "";
-  let questionCounter = 0;
-  const filteredQuestions = filteredData.filter((item) => item.dsatopic === topic);
-  const ques =
-    filteredQuestions.length > 0 ? (
-      filteredQuestions.map((item) => {
-        if (item.dsatopic !== currentTopic) {
-          questionCounter = 0;
-          currentTopic = item.dsatopic;
-        }
-        questionCounter++;
-        return (
-          <RowQues
-            key={item.id}
-            id={item.id}
-            serialNo={questionCounter}
-            ques={item.problem}
-            url={item.URL}
-            note={item.note}
-            status={item.status}
-            onStatusChange={() => handleStatusChange(item.id)}
-            onNoteClick={() => handleNoteClick(item.id, item.note)}
-            onNoteChange={handleNoteChange}
-            onNoteSave={() => handleNoteSave(item.id)}
-            isEditing={editingNoteId === item.id}
-            noteValue={noteValue}
-          />
-        );
-      })
-    ) : (
-      <TableRow>
-        <TableCell colSpan={5} align="center">
-          No questions found for the selected topic.
-        </TableCell>
-      </TableRow>
-    );
 
   return (
     <div>
-      <Typography variant="h4" align="center">
-        <Typewriter
-          options={{
-            strings: [topic],
-            delay: 700,
-            deleteSpeed: 1000,
-            autoStart: true,
-            loop: true,
-            pauseFor: 500,
-          }}
-        />
+      <Typography variant='h4' align='center'>
+        {topic}
       </Typography>
       <StyledTableContainer>
-        <StyledTable aria-label="questions table">
+        <StyledTable aria-label='questions table'>
           <TableHead>
             <TableRow>
-              <TableCell align="center">S No.</TableCell>
+              <TableCell align='center'>Status</TableCell>
               <TableCell>Question</TableCell>
-              <TableCell align="center">Links</TableCell>
-              <TableCell align="center">Status</TableCell>
-              <TableCell align="center">Notes</TableCell>
+              <TableCell align='center'>Links</TableCell>
+              <TableCell align='center'>Notes</TableCell>
             </TableRow>
           </TableHead>
-          <TableBody>{ques}</TableBody>
+          <TableBody>
+            {loading ? (
+              <Box
+                className='loading-container'
+                display='flex'
+                justifyContent='center'
+                alignItems='center'
+                height='50vh'
+                width='100vw'
+                overflow='hidden'
+              >
+                <Box className='loading-spinner'>
+                  <ClimbingBoxLoader size={30} color='#eb1777' />
+                </Box>
+              </Box>
+            ) : filteredData.length === 0 ? (
+              <TableRow>
+                <TableCell colSpan={5} align='center'>
+                  No questions found for the selected topic.
+                </TableCell>
+              </TableRow>
+            ) : (
+              filteredData.map((item, index) => (
+                <RowQues
+                  key={item.id}
+                  id={item.id}
+                  serialNo={index + 1}
+                  ques={item.problem}
+                  url={item.URL}
+                  note={item.note}
+                  status={item.status}
+                  onStatusChange={handleStatusChange}
+                  onNoteSave={handleNoteSave} // Pass onNoteSave function
+                />
+              ))
+            )}
+          </TableBody>
         </StyledTable>
       </StyledTableContainer>
     </div>
