0728202fb4348c3ffdcf7a9414efd23bf8b5deec
piyush016 <piyushcool116@gmail.com>
2024-02-13 04:04:29 +0530

    Update: Proected Routes created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
 import { useEffect, useState } from "react";
 import { ToastContainer } from "react-toastify";
 import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
@@ -9,23 +14,28 @@ import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
-import ForgotPassword from "./components/ForgotPassword/ForgotPassword"
-import Profile from "./components/Profile/Profile";
+import ForgotPassword from "./components/ForgotPassword/ForgotPassword";
+import Profile from "./components/Profile/Profile"; // Modified import
 import Topic from "./components/Topic/Topic";
 import ReportProblem from "./components/ReportProblem/ReportProblem";
-
 import { auth } from "./firebase";
-import { Box } from "@mui/material";
+import {
+  Box,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  Button,
+} from "@mui/material";
 
-function App() {
+function ProtectedRoute({ element, path }) {
+  const navigate = useNavigate();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [showLoginDialog, setShowLoginDialog] = useState(false);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       setIsLoggedIn(!!user);
-      setUser(user);
       setLoading(false);
     });
 
@@ -35,46 +45,101 @@ function App() {
     };
   }, []);
 
-  const renderLoading = () => (
-    <Box
-      className="loading-container"
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      height="100vh"
-    >
-      <Box className="loading-spinner">
-        <ClimbingBoxLoader size={30} color="#eb1777" />
+  if (loading) {
+    return (
+      <Box
+        className='loading-container'
+        display='flex'
+        justifyContent='center'
+        alignItems='center'
+        height='100vh'
+      >
+        <Box className='loading-spinner'>
+          <ClimbingBoxLoader size={30} color='#eb1777' />
+        </Box>
       </Box>
-    </Box>
-  );
+    );
+  }
 
-  const renderContent = () => {
-    if (loading) {
-      return renderLoading();
-    } else {
-      return (
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="forgot-password" element={<ForgotPassword/>}/>
-          <Route exact path="/signup" element={<Signup />} />
-          <Route
-            exact
-            path="/profile"
-            element={<Profile userId={user?.uid} />}
-          />
-          <Route exact path="/topic" element={<Topic />} />
-          <Route exact path="/report-problem" element={<ReportProblem user={user}/>} />
-        </Routes>
-      );
-    }
-  };
+  if (!isLoggedIn) {
+    return (
+      <>
+        <div
+          style={{
+            backdropFilter: "blur(10px)",
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            zIndex: 999,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {element}
+        </div>
+        <Dialog open={!isLoggedIn} onClose={() => setShowLoginDialog(false)}>
+          <DialogContent>
+            <div>You need to log in to access this page.</div>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => navigate("/")} color='primary'>
+              Close
+            </Button>
+            <Button
+              onClick={() => {
+                setShowLoginDialog(false);
+                navigate("/login");
+              }}
+              color='primary'
+              autoFocus
+            >
+              Log In
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </>
+    );
+  }
+
+  return element;
+}
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setIsLoggedIn(!!user);
+    });
+
+    // Clean up the subscription
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   return (
     <Router>
-      {renderContent()}
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/about' element={<About />} />
+        <Route exact path='/login' element={<Login />} />
+        <Route exact path='/forgot-password' element={<ForgotPassword />} />
+        <Route exact path='/signup' element={<Signup />} />
+        <Route
+          exact
+          path='/profile'
+          element={<Profile userId={auth.currentUser?.uid} />}
+        />
+        <Route path='/topic' element={<ProtectedRoute element={<Topic />} />} />
+        <Route
+          path='/report-problem'
+          element={<ProtectedRoute element={<ReportProblem />} />}
+        />
+      </Routes>
       <NavigationBar isLoggedIn={isLoggedIn} />
       <ToastContainer />
     </Router>
