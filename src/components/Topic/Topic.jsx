7cd7651880377ca78aaa5240f408198dc2bbc4cb
piyush016 <piyushcool116@gmail.com>
2024-02-13 04:07:51 +0530

    Add: Go back button

--- src/components/Topic/Topic.jsx
+++ src/components/Topic/Topic.jsx
@@ -2,14 +2,13 @@ import React, { useState } from "react";
 import TopicCard from "../TopicCard/TopicCard";
 import TableView from "../Table/Table";
 import { datatopic } from "./data";
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import "./Topic.css";
 import { Button } from "@mui/material";
 
 const Topic = () => {
   const [selectedTopic, setSelectedTopic] = useState(null);
   const handleTopicSelection = (topic) => {
-    console.log("Selected Topic:", topic);
     setSelectedTopic(topic);
   };
 
@@ -20,7 +19,7 @@ const Topic = () => {
   const cards = datatopic.map((item) => {
     return (
       <TopicCard
-        className="h-100"
+        className='h-100'
         key={item.id}
         topic={item.dsatopic}
         text={item.text}
@@ -35,12 +34,20 @@ const Topic = () => {
     <>
       {selectedTopic ? (
         <>
-          <Button variant="outlined" color="secondary"   onClick={handleGoBack} sx={{borderRadius: "10%", margin:"0.5rem 0rem 0rem 0.5rem"}}> <ArrowBackIosIcon/></Button>
+          <Button
+            variant='outlined'
+            color='secondary'
+            onClick={handleGoBack}
+            sx={{ borderRadius: "10%", margin: "0.5rem 0rem 0rem 0.5rem" }}
+          >
+            {" "}
+            <ArrowBackIosIcon />
+          </Button>
           <TableView topic={selectedTopic} />
         </>
       ) : (
-        <div className="cards d-flex">
-          <section className="mx-3 h-100 row row-cols-1 row-cols-md-2 row-cols-lg-3 mt-3 grids">
+        <div className='cards d-flex'>
+          <section className='mx-3 h-100 row row-cols-1 row-cols-md-2 row-cols-lg-3 mt-3 grids'>
             {cards}
           </section>
         </div>
