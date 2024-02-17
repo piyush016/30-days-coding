bedaf6e9f8471b037e534d7c9dab7f30d06e1ab3
piyush016 <piyushcool116@gmail.com>
2024-02-17 18:06:52 +0530

    Fix: Navigation of topics

--- src/components/Topic/Topic.jsx
+++ src/components/Topic/Topic.jsx
@@ -1,21 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import TopicCard from "../TopicCard/TopicCard";
-import TableView from "../Table/Table";
 import { datatopic } from "./data";
-import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import "./Topic.css";
-import { Button } from "@mui/material";
 
 const Topic = () => {
-  const [selectedTopic, setSelectedTopic] = useState(null);
-  const handleTopicSelection = (topic) => {
-    setSelectedTopic(topic);
-  };
-
-  const handleGoBack = () => {
-    setSelectedTopic(null);
-  };
-
   const cards = datatopic.map((item) => {
     return (
       <TopicCard
@@ -25,33 +13,17 @@ const Topic = () => {
         text={item.text}
         link={item.link}
         url={item.url}
-        onSelectTopic={handleTopicSelection}
       />
     );
   });
 
   return (
     <>
-      {selectedTopic ? (
-        <>
-          <Button
-            variant='outlined'
-            color='secondary'
-            onClick={handleGoBack}
-            sx={{ borderRadius: "10%", margin: "0.5rem 0rem 0rem 0.5rem" }}
-          >
-            {" "}
-            <ArrowBackIosIcon />
-          </Button>
-          <TableView topic={selectedTopic} />
-        </>
-      ) : (
-        <div className='cards d-flex'>
-          <section className='mx-3 h-100 row row-cols-1 row-cols-md-2 row-cols-lg-3 mt-3 grids'>
-            {cards}
-          </section>
-        </div>
-      )}
+      <div className='cards d-flex'>
+        <section className='mx-3 h-100 row row-cols-1 row-cols-md-2 row-cols-lg-3 mt-3 grids'>
+          {cards}
+        </section>
+      </div>
     </>
   );
 };
--- src/components/TopicCard/TopicCard.jsx
+++ src/components/TopicCard/TopicCard.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 import "./TopicCard.css";
+import { useNavigate } from "react-router";
 
 const TopicCard = (props) => {
+  const navigate = useNavigate();
   const handleShowQuestions = () => {
-    props.onSelectTopic(props.topic); // Call the prop function to pass the selected topic
+    navigate(`/topic/${props.topic}`);
   };
 
   return (
-    <div className="testimotionals">
-      <div className="card">
-        <div className="layer"></div>
-        <div className="content">
-          <div className="details">
+    <div className='testimotionals'>
+      <div className='card'>
+        <div className='layer'></div>
+        <div className='content'>
+          <div className='details'>
             <h2>{props.topic}</h2>
           </div>
           <p>{props.text}</p>
-          <div className="button-cont">
-            <div className="panel blue">
-              <a href={props.link} target="_blank" rel="noreferrer">
+          <div className='button-cont'>
+            <div className='panel blue'>
+              <a href={props.link} target='_blank' rel='noreferrer'>
                 <button>Learn More?</button>
               </a>
             </div>
-            <div className="panel pink">
-              {/* <a href={props.url}> */}
-                <button onClick={handleShowQuestions}>Solve Questions?</button>
-              {/* </a> */}
+            <div className='panel pink'>
+              <button onClick={handleShowQuestions}>Solve Questions?</button>
             </div>
           </div>
         </div>
